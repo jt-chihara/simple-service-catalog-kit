06b723f26ce030a9c1e95c1d5d73daefb8d4a447
jt-chihara <non>
2025-12-28 22:06:56 +0900

    add init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Service Dependency Catalog</h1>
+      <p>Loading...</p>
+    </div>
+  )
+}
+
+export default App
